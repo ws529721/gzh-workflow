f9d1d9cb7dacb192dc2e4d4b40e2a31e516f5970
ws529721 <2309364519@qq.com>
2026-07-27 23:41:01 +0800

    pwa fix sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'gzh-ai-v2';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))) });
